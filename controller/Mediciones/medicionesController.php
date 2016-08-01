d5528e0ee973d63e2ce50261c2daa7aae9ea9d20
TeyanWar <esteban.9411@hotmail.com>
2016-08-01 09:48:23 -0500

    Avances unificacion nicolas

--- controller/Mediciones/medicionesController.php
+++ controller/Mediciones/medicionesController.php
@@ -3,94 +3,124 @@
 include_once '../model/Mediciones/medicionesModel.php';
 include_once '../model/Personas/personasModel.php';
 include_once '../model/Equipos/equiposModel.php';
+include_once '../model/Medidores/medidoresModel.php';
 
 class MedicionesController {
-    
-    public function crear(){
-        
+
+    public function crear() {
+
         $objEquipos = new EquiposModel();
         $sql = "SELECT * FROM pag_equipo";
         $equipos = $objEquipos->select($sql);
         $objEquipos->cerrar();
-        
+
         $objPersona = new personasModel();
         $sql = "SELECT * FROM pag_persona";
         $personas = $objPersona->select($sql);
         $objPersona->cerrar();
         
+        $objMedidor= new medidoresModel();
+        $sql= "SELECT tmed_id,tmed_acronimo from pag_tipo_medidor where tmed_estado=0";
+        $medidores= $objMedidor->select($sql);
+        $_SESSION['medidores']=$medidores;
+        $objMedidor->cerrar();
+
         include_once '../view/Mediciones/mediciones/crear.html.php';
     }
-    
-    public function postCrear(){
-        //die(print_r($_POST));
-        $fechaMedicion = $_POST['fecha'];
-        $medidaActual = $_POST['medidaActual'];
-        $equipos = $_POST['equipos'];
-        $personas = $_POST['personas'];
-        $objMediciones = new MedicionesModel();
-        $sql = "INSERT INTO pag_control_medidas (ctrmed_fecha, ctrmed_medida_actual, equi_id, per_id)"
-                . "VALUES ('$fechaMedicion',"
-                . "'$medidaActual',"
-                . "'$equipos',"
-                . "$personas)";
-        
-        //die($sql);
-        $mediciones = $objMediciones->insertar($sql);
-        $objMediciones->cerrar();       
-        redirect(crearUrl('mediciones', 'mediciones', 'listar'));
-    }
-    
-    public function editar($parametros=false){
+
+   
+    public function editar($parametros = false) {
         $objMediciones = new MedicionesModel();
         $id = $parametros[1];
 
         $sql = "SELECT pag_control_medidas.ctrmed_id,pag_persona.per_id,"
-            . "pag_persona.per_nombre,pag_equipo.equi_id,pag_equipo.equi_nombre,"
-            . "pag_control_medidas.ctrmed_fecha,pag_control_medidas.ctrmed_medida_actual "
-            . "FROM pag_persona,pag_equipo,pag_control_medidas "
-            . "WHERE pag_control_medidas.per_id=pag_persona.per_id "
-            . "AND pag_control_medidas.equi_id=pag_equipo.equi_id "
-            . "AND pag_control_medidas.ctrmed_id=$id";
-        
+                . "pag_persona.per_nombre,pag_equipo.equi_id,pag_equipo.equi_nombre,"
+                . "pag_control_medidas.ctrmed_fecha,pag_control_medidas.ctrmed_medida_actual "
+                . "FROM pag_persona,pag_equipo,pag_control_medidas "
+                . "WHERE pag_control_medidas.per_id=pag_persona.per_id "
+                . "AND pag_control_medidas.equi_id=pag_equipo.equi_id "
+                . "AND pag_control_medidas.ctrmed_id=$id";
+
         $mediciones = $objMediciones->find($sql);
         $objMediciones->cerrar();
         include_once '../view/Mediciones/mediciones/editar.html.php';
     }
-    
-    public function postEditar(){
-        //die(print_r($_POST));
-        $id = $_POST['id'];
-        $equipo = $_POST['equipo'];
-        $persona = $_POST['persona'];
-        $medidaActual = $_POST['medidaActual'];
-        $fechaMedicion = $_POST['fechaMedicion'];
-        
-        $objMediciones = new MedicionesModel();
-        $sql = "UPDATE pag_control_medidas SET "
-                . "ctrmed_fecha = '$fechaMedicion',"
-                . "ctrmed_medida_actual = '$medidaActual',"
-                . "equi_id = '$equipo',"
-                . "per_id = $persona "
-                . "WHERE ctrmed_id = $id";
 
-        $mediciones = $objMediciones->update($sql);
-        
+    public function postEditar() {
+        //die(print_r($_POST));
+
+        $errores = array();
+        $patronLetras = "/^[a-zA-Z_áéíóúñ\s]*$/";
+        $patronNumeros = "/^[0-9]*$/";
+
+        if (!isset($_POST['id']) or $_POST['id'] == "") {
+            $errores[] = "Debe existir el Id, no puede estar vacio";
+        }
+        if(isset($_POST['id']) && !preg_match($patronNumeros,$_POST['id'])){
+            $errores[]="El id debe ser numerico";
+        }
+        if (!isset($_POST['equipo']) or $_POST['equipo'] == "") {
+            $errores[] = "El campo Equipo no puede estar vacio";
+        }
+        if (isset($_POST['equipo']) && !is_numeric($_POST['equipo'])) {
+            $errores[] = "El campo Equipo admiten Letras y numeros Unicamente";
+        }
+        if (!isset($_POST['medidaActual']) or $_POST['medidaActual'] == "") {
+            $errores[] = "El campo Medida no puede estar vacio";
+        }
+        if (isset($_POST['medidaActual']) && !is_numeric($_POST['medidaActual'])) {
+            $errores[] = "El campo Medida debe ser numerico unicamente";
+        }
+        if (count($errores) > 0) {
+            setErrores($errores);
+            redirect(crearUrl('mediciones', 'mediciones', 'listar'));
+        } else {
+            $id = $_POST['id'];
+            $equipo = $_POST['equipo'];
+            $persona = $_POST['persona'];
+            $medidaActual = $_POST['medidaActual'];
+            explodeFecha($_POST['fechaMedicion']);
+            $fechaMedicion=  getFecha();
+            $objMediciones = new MedicionesModel();
+
+            $sql = "UPDATE pag_control_medidas SET "
+                    . "ctrmed_fecha = '$fechaMedicion',"
+                    . "ctrmed_medida_actual = '$medidaActual',"
+                    . "equi_id=$equipo,"
+                    . "per_id = $persona "
+                    . "WHERE ctrmed_id = $id";
+            $mediciones = $objMediciones->update($sql);
+            redirect(crearUrl('mediciones', 'mediciones', 'listar'));
+        }
+
         $objMediciones->cerrar();
-        redirect(crearUrl('mediciones', 'mediciones', 'listar'));
     }
-    
-    public function listar(){
+
+    public function listar() {
         $objMediciones = new MedicionesModel();
-        $sql = "SELECT * FROM pag_control_medidas, pag_persona, pag_equipo WHERE pag_control_medidas.per_id=pag_persona.per_id AND
-                pag_control_medidas.equi_id=pag_equipo.equi_id";
-       
+        $sql = "SELECT * FROM pag_control_medidas, pag_persona, pag_equipo,pag_tipo_medidor WHERE pag_control_medidas.per_id=pag_persona.per_id AND
+                pag_control_medidas.equi_id=pag_equipo.equi_id and pag_control_medidas.tmed_id=pag_tipo_medidor.tmed_id order by ctrmed_fecha desc ";
+
         $mediciones = $objMediciones->select($sql);
-         
+
+        /*
+         * Paginado
+         */
+        $pagina = (isset($_REQUEST['pagina'])?$_REQUEST['pagina']:1); 
+        $url = crearUrl('mediciones', 'mediciones', 'listar');
+        
+        $paginado = new Paginado($mediciones, $pagina, $url);
+        
+        $mediciones = $paginado->getDatos();
+        /*
+         * Fin paginado
+         */
+        
         $objMediciones->cerrar();
         include_once '../view/Mediciones/mediciones/listar.html.php';
     }
-    
-    public function eliminar($parametros){
+
+    public function eliminar($parametros) {
         $objMediciones = new MedicionesModel();
         $id = $parametros[0];
         $sql = "DELETE * FROM pag_control_medidas WHERE ctrmed_id = $id";
@@ -98,8 +128,8 @@ class MedicionesController {
         $objMediciones->cerrar();
         include_once '../view/Mediciones/mediciones/listar.html.php';
     }
-    
-    public function postEliminar(){
+
+    public function postEliminar() {
         $id = $_POST['id'];
         $objMediciones = new MedicionesModel();
         $sql = "DELETE  FROM pag_control_medidas WHERE ctrmed_id = $id";
@@ -108,47 +138,114 @@ class MedicionesController {
         redirect(crearUrl('mediciones', 'mediciones', 'listar'));
     }
 
-    public function ajaxAgregarEquipo(){
+    public function ajaxAgregarEquipo() {
         $idsEquipos = $_POST['ids'];
         $equipos = array();
         $objEquipos = new EquiposModel();
-        foreach($idsEquipos as $idEquipo){
+        foreach ($idsEquipos as $idEquipo) {
             $sql = "SELECT equi_id, equi_nombre FROM pag_equipo WHERE equi_id = $idEquipo";
             $equipo = $objEquipos->select($sql);
             $equipos[$equipo[0]['equi_id']] = $equipo[0];
         }
-        
+
         $objEquipos->cerrar();
         include_once '../view/Mediciones/mediciones/ajaxAgregarEquipo.html.php';
     }
-    
-    public function ajaxListarEquipos(){
+
+    public function ajaxListarEquipos() {
+        $objMedidores=New MedidoresModel();
+        
         $equi_id = $_POST['equi_id'];
         $equi_nombre = $_POST['equi_nombre'];
         $medicion = $_POST['medicion'];
         $fecha = $_POST['fecha'];
+        $tipoMedidor = $_POST['tipo_medidor'];
+        $consecutivo = $_POST['consecutivo'];
+        
+        $sql = "Select * from pag_tipo_medidor";
+                
+        $medidores=$objMedidores->select($sql);
+        
         include_once '../view/Mediciones/mediciones/ajaxListarEquipos.html.php';
     }
-    
-    
-    public function ajaxGuardarMedidas(){
-        $medidas = $_POST['medidas'];
-        $personaId = $_POST['personas'];
-        $objMediciones = new MedicionesModel();
-        
-        foreach($medidas as $medida){
-        
-            $sql = "INSERT INTO pag_control_medidas (ctrmed_fecha, ctrmed_medida_actual, equi_id, per_id)"
-                    . "VALUES ('$personaId',"
-                    . "'$medida[equi_id]',"
-                    . "'$medida[medicion]',"
-                    . "'$medida[fecha]')";
-            
-            $objMediciones->insertar($sql);
-            die($sql);
+
+    public function ajaxGuardarMedidas() {
+        $errores = array();
+        $patronLetras = "/^[a-zA-Z_áéíóúñ\s]*$/";
+        $patronLetrasNumeros = "/^[0-9a-zA-Z]+$/";
+
+        if (!isset($_POST['personas']) or $_POST['personas'] == "") {
+            $errores[] = "El campo Responsable es Obligatorio";
         }
-        
-        $objMediciones->cerrar();
-        redirect(crearUrl('mediciones', 'mediciones', 'listar'));
+        if (isset($_POST['personas']) && !is_numeric($_POST['personas'])) {
+            $errores[] = "En el campo Responsable unicamente se aceptan letras";
+        }
+        if (!isset($_POST['equipos']) or $_POST['equipos'] == "") {
+            $errores[] = "El campo Equipo no puede estar vacio";
+        }
+        if (isset($_POST['equipos']) && !is_numeric($_POST['equipos'])) {
+            $errores[] = "En el campo Equipo unicamente se aceptan letras";
+        }
+        if (!isset($_POST['medidaActual']) or $_POST['medidaActual'] == "") {
+            $errores[] = "El campo Medidas no puede estar vacio";
+        } else {
+            if (isset($_POST['medidaActual']) && !is_numeric($_POST['medidaActual'])) {
+                $errores[] = "En el campo Medidas unicamente se aceptan Numeros";
+            }
+        }
+        if (!isset($_POST['fecha']) or $_POST['fecha'] == "") {
+            $errores[] = "El campo Fecha no puede estar vacio";
+        } else {
+//            if (isset($_POST['fecha']) && !preg_match($patronLetrasNumeros,$_POST['fecha'])) {
+//                $errores[] = "En el campo Fecha unicamente se aceptan Letras y Numeros";
+//            }
+        }
+        if (!isset($_POST['medidas']) or $_POST['medidas'] == "") {
+            $errores[] = "Debe agregar al menos 1 medicion";
+        } else {
+            $medidas = $_POST['medidas'];
+            foreach ($medidas as $medida) {
+                if ($medida['medicion'] == "") {
+                    $errores[] = "El campo Medidas no puede estar vacio";
+                } else {
+                    if (!is_numeric($medida['medicion'])) {
+                        $errores[] = "En el campo Medidas unicamente se aceptan Numeros";
+                    }
+                    if (!is_numeric($medida['equi_id'])) {
+                        $errores[] = "El codigo del equipo debe ser Numerico unicamente";
+                    }
+                    if (!preg_match($patronLetras, $medida['equi_nombre'])) {
+                        $errores[] = "SOLO LETRAS";
+                    }
+                    if (!is_numeric($medida['tipo_medidor'])) {
+                        $errores[] = "SOLO Numeros";
+                    }
+                }
+            }
+        }
+        if (count($errores) > 0) {
+            setErrores($errores);
+            redirect(crearUrl('mediciones', 'mediciones', 'crear'));
+        } else {
+            $medidas = $_POST['medidas'];
+            $personaId = $_POST['personas'];
+            $objMediciones = new MedicionesModel();
+
+            foreach ($medidas as $medida) {
+                explodeFecha($medida['fecha']);
+                $fecha=  getFecha();
+                $sql = "INSERT INTO pag_control_medidas (per_id,equi_id,ctrmed_medida_actual,ctrmed_fecha,tmed_id)"
+                        . "VALUES ('$personaId',"
+                        . "'$medida[equi_id]',"
+                        . "'$medida[medicion]',"
+                        . "'$fecha',"
+                        . "'$medida[tipo_medidor]')";
+                $objMediciones->insertar($sql);
+            }
+
+            $objMediciones->cerrar();
+            redirect(crearUrl('mediciones', 'mediciones', 'listar'));
+        }
     }
+
 }
--- controller/Medidores/medidoresController.php
+++ controller/Medidores/medidoresController.php
@@ -10,20 +10,58 @@ class MedidoresController {
     
     public function postCrear(){
         //die(print_r($_POST));
-        $nombre = $_POST['nombre'];
-        $acronimo = $_POST['acronimo'];
-        $descripcion = $_POST['descripcion'];
-        $estado = $_POST['estado'];
-        $objMedidores = new MedidoresModel();
-        $sql = "INSERT INTO pag_tipo_medidor (tmed_nombre, tmed_acronimo, tmed_descripcion, tmed_estado)"
-                . "VALUES ('$nombre',"
-                . "'$acronimo',"
-                . "'$descripcion',"
-                . "'$estado')";
-        //die($sql);
-        $medidores = $objMedidores->insertar($sql);
-        $objMedidores->cerrar();
-        redirect(crearUrl('medidores', 'medidores', 'listar'));
+        $errores=array();
+        $patronLetras="/^[a-zA-Z_áéíóúñ\s]*$/";
+        $patronLetrasNumeros="/^[0-9a-zA-Z]+$/";
+        
+        if(!isset($_POST['nombre']) or $_POST['nombre']==""){
+            $errores[]="El campo nombre no puede estar vacio";
+        }//Valida que el campo nombre medidor no llegue vacio
+        
+        if(isset($_POST['nombre']) && !preg_match($patronLetras,$_POST['nombre'])){
+            $errores[]="En el campo nombre unicamente se admiten Letras";
+        }//Valida que el campo nombre medidor contenga letras unicamente
+        
+        if(!isset($_POST['acronimo']) or $_POST['acronimo']==""){
+            $errores[]="El campo Acronimo no puede estar vacio";
+        }//Valida que el campo acronimo no llegue vacio
+        
+        if(isset($_POST['acronimo']) && !preg_match($patronLetras,$_POST['acronimo'])){
+            $errores[]="En el campo Acronimo unicamente se admiten Letras";
+        }//Valida que el campo acronimo contenga letras unicamente
+        
+        if(isset($_POST['acronimo']) && strlen($_POST['acronimo'])>3){
+            $errores[]="El campo acronimo no debe sobrepasar los 4 caracteres";
+        }//Valida que el campo acronimo contenga letras unicamente
+        
+
+        if(!isset($_POST['descripcion']) or $_POST['descripcion']==""){
+            $errores[]="El campo descripcion no puede estar vacio";
+        }//Valida que el campo acronimo no llegue vacio
+        
+        if(isset($_POST['descripcion']) && !preg_match($patronLetrasNumeros,$_POST['descripcion'])){
+            $errores[]="En el campo descripcion unicamente se admiten letras y numeros";
+        }//Valida que el campo acronimo contenga letras unicamente
+        
+        
+        if(count($errores)>0){
+            setErrores($errores);
+            redirect(crearUrl('medidores', 'medidores', 'crear'));
+        }else{
+        
+            $nombre = $_POST['nombre'];
+            $acronimo = $_POST['acronimo'];
+            $descripcion = $_POST['descripcion'];
+            $objMedidores = new MedidoresModel();
+            $sql = "INSERT INTO pag_tipo_medidor (tmed_nombre, tmed_acronimo, tmed_descripcion,tmed_estado)"
+                    . "VALUES ('$nombre',"
+                    . "'$acronimo',"
+                    . "'$descripcion',"
+                    . "0)";
+            $medidores = $objMedidores->insertar($sql);
+            $objMedidores->cerrar();
+            redirect(crearUrl('medidores', 'medidores', 'listar'));
+        }
     }
     
     public function editar($parametros=false){
@@ -40,29 +78,80 @@ class MedidoresController {
     }
     
     public function postEditar(){
+        $errores=array();
+        $patronLetras="/^[a-zA-Z_áéíóúñ\s]*$/";
+        $patronLetrasNumeros="/^[0-9a-zA-Z]+$/";
+        
+        if(!isset($_POST['tmed_nombre']) or $_POST['tmed_nombre']==""){
+            $errores[]="El campo nombre no puede estar vacio";
+        }//Valida que el campo nombre medidor no llegue vacio
+        
+        if(isset($_POST['tmed_nombre']) && !preg_match($patronLetras,$_POST['tmed_nombre'])){
+            $errores[]="En el campo nombre unicamente se admiten Letras";
+        }//Valida que el campo nombre medidor contenga letras unicamente
+        
+        if(!isset($_POST['tmed_acronimo']) or $_POST['tmed_acronimo']==""){
+            $errores[]="El campo Acronimo no puede estar vacio";
+        }//Valida que el campo acronimo no llegue vacio
+        
+        if(isset($_POST['tmed_acronimo']) && !preg_match($patronLetras,$_POST['tmed_acronimo'])){
+            $errores[]="En el campo Acronimo unicamente se admiten Letras";
+        }//Valida que el campo acronimo contenga letras unicamente
+        
+        if(isset($_POST['tmed_acronimo']) && strlen($_POST['tmed_acronimo'])>3){
+            $errores[]="El campo acronimo no debe sobrepasar los 4 caracteres";
+        }//Valida que el campo acronimo contenga letras unicamente
+        
+        if(!isset($_POST['tmed_descripcion']) or $_POST['tmed_descripcion']==""){
+            $errores[]="El campo descripcion no puede estar vacio";
+        }//Valida que el campo acronimo no llegue vacio
+        
+        if(isset($_POST['tmed_descripcion']) && !preg_match($patronLetras,$_POST['tmed_descripcion'])){
+            $errores[]="En el campo descripcion unicamente se admiten letras y numeros";
+        }//Valida que el campo acronimo contenga letras unicamente
+        
+        
+        if(count($errores)>0){
+            setErrores($errores);
+            redirect(crearUrl('medidores', 'medidores', 'listar'));
+        }else{
+        
         $id = $_POST['id'];
         $tmed_nombre = $_POST['tmed_nombre'];
         $tmed_acronimo = $_POST['tmed_acronimo'];
         $tmed_descripcion = $_POST['tmed_descripcion'];
-        $tmed_estado = $_POST['tmed_estado'];
         $objMedidores = new MedidoresModel();
         
         $sql = "UPDATE pag_tipo_medidor SET "
                 . "tmed_nombre = '$tmed_nombre',"
                 . "tmed_acronimo = '$tmed_acronimo',"
-                . "tmed_descripcion = '$tmed_descripcion',"
-                . "tmed_estado = '$tmed_estado' WHERE tmed_id = $id";
+                . "tmed_descripcion = '$tmed_descripcion'"
+                . " WHERE tmed_id = $id";
 //        die($sql);
+        
         $medidores = $objMedidores->update($sql);
         $objMedidores->cerrar();
         redirect(crearUrl('medidores', 'medidores', 'listar'));
+        }
     }
     
     public function listar(){
         $objMedidores = new MedidoresModel();
-        $sql = "SELECT * FROM pag_tipo_medidor WHERE tmed_estado = 'Activo'";
+        $sql = "SELECT * FROM pag_tipo_medidor WHERE tmed_estado=0";
         $medidores = $objMedidores->select($sql);
-
+        
+        /*
+         * Paginado
+         */
+        $pagina = (isset($_REQUEST['pagina'])?$_REQUEST['pagina']:1); 
+        $url = crearUrl('medidores', 'medidores', 'listar');
+        
+        $paginado = new Paginado($medidores, $pagina, $url);
+        
+        $medidores = $paginado->getDatos();
+        /*
+         * Fin paginado
+         */
         $objMedidores->cerrar();
         include_once '../view/Medidores/medidores/listar.html.php';
     }
