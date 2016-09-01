79bde7bb5548b49fd474fd889dff8a2b82a51f4a
NRodriguez98 <nick-2642@hotmail.com>
2016-08-31 20:32:13 -0500

    Adelanto modulo mediciones

--- view/Mediciones/mediciones/listar.html.php
+++ view/Mediciones/mediciones/listar.html.php
@@ -19,32 +19,85 @@
             </button>
         </div>
         <?php
-        $errores = getErrores();
-        if (!$errores == "") {
-            ?>
-            <div id="prueba">
-                <div id="card-alert" class="card red">
-                    <div class="card-content white-text">
-                        <p><i class="mdi-alert-error"></i> 
-                        <p><?php echo $errores ?></p>
+            $errores = getErrores();
+            if (!$errores == "") { ?>
+                <div id="prueba">
+                    <div id="card-alert" class="card red">
+                        <div class="card-content white-text">
+                            <p><i class="mdi-alert-error"></i> 
+                            <p><?php echo $errores ?></p>
+                        </div>
+                        <button type="button" class="close white-text" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">×</span>
+                        </button>
                     </div>
-                    <button type="button" class="close white-text" data-dismiss="alert" aria-label="Close">
-                        <span aria-hidden="true">×</span>
-                    </button>
                 </div>
-            </div>
         <?php } ?>
 
-        <div id="right-search" class="row">
-            <form class="col s12">
-                <div class="input-field">
-                    <i class="mdi-action-search prefix"></i>
-                    <input type="text" class="validate" id="buscarMed" name='buscarMed' class="header-search-input z-depth-2" data-url="<?php echo crearUrl("Mediciones", "Mediciones", "buscador", array('noVista' => "noVista")) ?>" />
-                    <label for="icon_prefix" class="active">Buscador de Mediciones</label>
-                </div>
-            </form>
+        <!--Fin mensaje de campos obligatorios-->
+
+        <table class="highlight center striped bordered">
+            <div class="row">
+                <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Equipo</th>
+                    <th>Últimas Mediciones</th>
+                    <th>Total</th>
+                    <th>Opciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $count = 1;
+                foreach ($equipos as $equipo) { ?>
+                    <tr>
+                        <td><?php echo $count++; ?></td>
+                        <td><?php echo $equipo['equi_nombre'] ?></td>
+                        <td>
+                            <?php foreach($equipo['tiposMedidores'] as $tipoMedidor){ 
+                                echo $tipoMedidor['tmed_nombre'].": ".$tipoMedidor['ultimaMedicion']['ctrmed_medida_actual']
+                                     ." ".$tipoMedidor['ultimaMedicion']['responsable']." ".$tipoMedidor['ultimaMedicion']['ctrmed_fecha']."<br>"; 
+                            } ?>
+                        </td>
+                        <td>
+                            <?php foreach($equipo['tiposMedidores'] as $tipoMedidor){ 
+                                echo $tipoMedidor['totalMediciones']."<br>";
+                            } ?>
+                        </td>
+                        <td><a class="btn-floating waves-effect waves-light modal-trigger cyan darken-1" 
+                                   href="#modal_detalle_mediciones" 
+                                   data-url="<?php echo crearUrl("Mediciones", "mediciones", "detalle", array('noVista' => 'noVista', 'id' => $equipo['equi_id'])); ?>"><i class="mdi-action-find-in-page tiny"></i></a>
+                        </td>
+                    </tr>
+            <?php } ?>
+            </tbody>
+            </div>
+            
+        </table>
+        
+        
+        <div class="modal" id="modal_detalle_mediciones">
+            <div class="modal-content ">
+
+            </div> 
         </div>
         
-        <div id="listadoMediciones">
-            
-        </div>
+        <?php // $paginado->render() ?>
+        
+    </div>
+</div>
+<script type="text/javascript">
+    $('.modal-trigger').leanModal({
+        dismissible: true, // Modal can be dismissed by clicking outside of the modal
+        opacity: .5, // Opacity of modal background
+        in_duration: 300, // Transition in duration
+        out_duration: 200, // Transition out duration
+        ready: function () {
+            //alert('Ready'); 
+        }, // Callback for Modal open
+        complete: function () {
+            //alert('Closed'); 
+        } // Callback for Modal close
+    });
+</script>
