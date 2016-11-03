edd5f06961f08ec8966e35ad70918fbc4b0301df
Diego Velez <diego_Fernando97@outlook.es>
2016-11-03 00:24:54 -0500

    Js de costos

--- web/js/modulos/costos-costos.js
+++ web/js/modulos/costos-costos.js
@@ -1,14 +1,28 @@
-$("#filtroFecha").click(function (e) {
-    $("#respuestaCostos").html("Cargando...");
-    e.preventDefault();
-    var url = $(this).attr("data-url");
 
-    $.ajax({
-        url: url,
-        type: "POST",
-        data: $("#formCostosAjax").serialize(),
-        success: function (data) {
-            $("#respuestaCostos").html(data);
+$(document).ready(function () {
+ $("#buscaOt").keyup(function () {
+        var OT = $("#buscaOt").val();
+            
+        if (OT != "") {
+            $('#pagina').val(1);
         }
+        var pagina = $('#pagina').val(); 
+        var url = $(this).attr("data-url");
+        $.ajax({
+            url: url,
+            type: "POST",
+            data: "OT=" + OT +"&pagina"+pagina,
+            success: function (data) {
+                $("#tablaOt").html(data);
+            }
+        });
     });
-});
+
+    $("#buscaOt").trigger('keyup');
+    
+   //-------------funcion pdf-------------------------
+    $(document).on('click', '.pdf', function () {
+        document.formulariopdf.submit()
+    });
+
+ });
